157bf71bc74d710f58a0c63f8a3fae031e7e1ed9
pawankumar <pawan221b@gmail.com>
2019-05-16 20:43:07 +0530

    Stful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: HomePage(),
-    ),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-// * Stateless Widget // * Stateful WIdget
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+  var myText = "Change me";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My App"),
+        title: Text("Stateful Widget"),
       ),
       body: Center(
         child: Text(
-          "Hello World",
+          myText,
           style: TextStyle(
-            fontSize: 25,
+            fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountEmail: Text("mtechviral@gmail.com"),
-              accountName: Text("Pawan Kumar"),
-              currentAccountPicture: CircleAvatar(
-                child: Text("P"),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Image.network(
-                "https://m.telegraphindia.com/unsafe/620x350/smart/static.telegraphindia.com/derivative/THE_TELEGRAPH/1689224/16X9/image36e62462-75c0-45de-abd6-1a5b2677d0e6.jpg",
-                width: 50,
-                height: 30,
-              ),
-              title: Text("Dhoni"),
-              subtitle: Text("CSK"),
-              trailing: Icon(Icons.delete),
-            ),
-          ],
-        ),
+        child: Icon(Icons.change_history),
+        onPressed: () {
+          myText = Random().nextInt(10).toString();
+          setState(() {});
+        },
       ),
     );
   }
